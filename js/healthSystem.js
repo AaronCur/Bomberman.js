742fdb6db8cbdc965145d74458ea67ffe2674d79
AaronCur <c00202505@itcarlow.ie>
2019-01-22 11:23:11 +0000

    Added sprites

---
+++ js/healthSystem.js
@@ -0,0 +1,32 @@
+class HealthSystem
+{
+  /**
+   * Constructor function , creates a scene with parameters which are passed in on construction
+   * @param {String} title - passes in a string whihc is set in the main
+   */
+  constructor(title)
+  {
+    this.title = title;
+    this.windowWidth = window.innerWidth;
+    this.windowHeight = window.innerHeight;
+   this.bgimg = new Image();
+    this.bgimg.src ="img/gameoverbg.png"
+  }
+
+  /**
+   * render function which will overwrite the one inherited by scene
+   * it defines a font and its size along with the background colour
+   */
+  render()
+  {
+
+    var canvas = document.createElement("mycanvas");
+    var ctx = mycanvas.getContext("2d");
+    var imagebg = this.bgimg;
+    var image = this.img;
+    ctx.drawImage(imagebg, 0 , 0,this.windowWidth, this.windowHeight ,0,0, this.windowWidth ,this.windowHeight);
+
+  }
+
+
+}
